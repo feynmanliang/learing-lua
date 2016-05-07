9e724b61e920cae166946d4df25b908b03fde205
Feynman Liang <feynman.liang@gmail.com>
2016-05-07 17:16:09 +0100

    Completes tables example

--- tables.lua
+++ tables.lua
@@ -6,5 +6,6 @@ c = { [x] = b, ["string"] = 10, [34] = 10, [b] = x } -- variables and literals a
 
 t = { 24, 25, 8, 13, 1, 40 } -- arrays are tables with 1..#t keys (1 indexed)
 table.insert(t, 50) -- append
---table.insert(t, 3, 89) -- inserts at position 3, shifting rest of array right
-
+table.insert(t, 3, 89) -- inserts at position 3, shifting rest of array right
+table.remove(t, 2) -- removes itema t index 2 (25)
+table.sort(t) -- sorts via the < operator
